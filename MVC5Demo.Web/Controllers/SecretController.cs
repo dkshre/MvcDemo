fd68aeb5c5f7e1db13ce9b9ffa2d1ca6dbd1935f
Dibesh Shrestha <dkshre@gmail.com>
2016-03-08 22:20:22 -0500

    Controller with Authorize attribute added

---
+++ MVC5Demo.Web/Controllers/SecretController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Web.Controllers
+{
+    public class SecretController: Controller
+    {
+        //Entity frameworks AspNetUsers table's UserName column
+        [Authorize(Users = "dkshre@gmail.com")]
+        public ContentResult Secret()
+        {
+            return Content("this is a secret.");
+        }
+        [AllowAnonymous]
+        public ContentResult Overt()
+        {
+            return Content("This is NOT a secret.");
+        }
+    }
+}
